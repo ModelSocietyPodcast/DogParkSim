6717d3ee6acf4cd9cd3e3e571ed9accf1e8d9831
ModelSocietyPodcast <stargardterm@gmail.com>
2025-01-04 19:48:00 -0500

    Update DogPark_SL_04Jan2025.py

--- DogPark_SL_04Jan2025.py
+++ DogPark_SL_04Jan2025.py
@@ -74,7 +74,7 @@ def main():
         background-image: url('MadisonSquarePark.png');
         background-size: cover;
         background-position: center;
-        opacity: 0;
+        opacity: 1;
     }
     </style>
     """,
